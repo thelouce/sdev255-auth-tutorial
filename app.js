01968b446c57a048cc611f1a279d8a7708c4dcfe
thelouce <100891923+thelouce@users.noreply.github.com>
2023-03-11 17:16:54 -0500

    finishing the auth tutorial

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes')
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware')
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -18,6 +21,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .catch((err) => console.log(err));
 
 // routes
+app.get('*', checkUser);
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,42 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 //handle errors 
 const handleErrors = (err) => {
     console.log(err.message, err.code);
+    let errors = { email: '', password: ''};
+
+    //incorrect email
+    if (err.message === 'incorrect email'){
+        errors.email = 'that email is not registered'
+    }
+
+     //incorrect email
+     if (err.message === 'incorrect password'){
+        errors.password = 'the password is not correct'
+    }
+
+    //duplicate error code
+    if (err.code === 11000) {
+        errors.email = 'that email is already registered';
+        return errors;
+    }
+
+    //validation errors
+    if (err.message.includes('user validation failed')) {
+        Object.values(err.errors).forEach(({properties}) => {
+            errors[properties.path] = properties.message;
+        });
+    }
+
+    return errors;
+}
+
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+    return jwt.sign({ id }, 'net ninja secret', {
+        expiresIn: maxAge
+    });
 }
 
 module.exports.signup_get = (req, res) => {
@@ -20,17 +54,31 @@ module.exports.signup_post = async (req, res) => {
     // res.send('new signup');
     try {
         const user = await User.create({ email, password });
-        res.staus(201).json(user);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000});
+        res.status(201).json({ user: user._id });
     }
     catch(err) {
-        console.log(err);
-        res.status(400).send('error, user not created');
+        const errors = handleErrors(err);
+        res.status(400).json({ errors });
     }
 }
 
 module.exports.login_post = async (req, res) => {
     const { email, password } = req.body;
 
-    console.log(email, password);
-    res.send('user login');
+    try {
+        const user = await User.login(email, password);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000});
+        res.status(200).json({ user: user._id });
+    } catch (err) {
+        const errors = handleErrors(err);
+        res.status(400).json({ errors });
+    }
+}
+
+module.exports.logout_get = (req, res) => {
+    res.cookie('jwt', '', { maxAge: 1 });
+    res.redirect('/');
 }
